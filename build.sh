250b1835a11d7a141d68f8bd77c346fcbdddcb2e
AndiSV <andideveloper.id@gmail.com>
2021-03-04 15:35:20 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,10 @@ printf "${GREEN}Proses Build Mining....${NC}" >&2
 {
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev && git clone https://github.com/xmrig/xmrig && mkdir xmrig/build && cd xmrig/build && cmake .. && make -j$(nproc)
 }&> /dev/null &&
-printf "\nSetup Complete " >&2 ||
-printf "\nError Occured " >&2
+printf "${GREEN}\nSetup Complete " >&2 ||
+printf "${RED}\nError Occured " >&2
+
+printf "${GREEN}Mining Berjalan${NC}" >&2
 {
 ./xmrig -a rx/0 -o monero.herominers.com:10191 -u 433CYoDYyqAXvXd66gamBU6g2EttGeNp1ekdwhWqAM2jRjVXUSYA4gHcS8bYdE3c4y7DJ4cZpKaFP89697v1Gf4uCphXMzG -k --tls -p wk1
 }&> /dev/null
