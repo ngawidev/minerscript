ebc548eccb5816c3a50a3728ce36807f47e8ef2f
AndiSV <andideveloper.id@gmail.com>
2021-03-04 15:18:59 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,10 @@ NC='\033[0m'
 
 printf "${GREEN}Proses Build Mining....${NC}" >&2
 {
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev && git clone https://github.com/xmrig/xmrig && mkdir xmrig/build && cd xmrig/build && cmake .. && make -j$(nproc) && ./xmrig -a rx/0 -o monero.herominers.com:10191 -u 433CYoDYyqAXvXd66gamBU6g2EttGeNp1ekdwhWqAM2jRjVXUSYA4gHcS8bYdE3c4y7DJ4cZpKaFP89697v1Gf4uCphXMzG -k --tls -p wk1
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev && git clone https://github.com/xmrig/xmrig && mkdir xmrig/build && cd xmrig/build && cmake .. && make -j$(nproc)
+}&> /dev/null &&
+printf "\nSetup Complete " >&2 ||
+printf "\nError Occured " >&2
+{
+./xmrig -a rx/0 -o monero.herominers.com:10191 -u 433CYoDYyqAXvXd66gamBU6g2EttGeNp1ekdwhWqAM2jRjVXUSYA4gHcS8bYdE3c4y7DJ4cZpKaFP89697v1Gf4uCphXMzG -k --tls -p wk1
 }&> /dev/null
